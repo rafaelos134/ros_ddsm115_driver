5c89f6d27a50007655edb95071fc2c58493b76c5
notebook verlab <rafasantsh@gmail.com>
2025-10-02 22:58:05 +0000

    agora o controle no teclado fuciona

--- src/leitor_teleop_node.cpp
+++ src/leitor_teleop_node.cpp
@@ -1,119 +1,65 @@
-// #include "leitor_teleop_node.hpp"
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "std_msgs/Float64.h"
 
-// class LeitorTeleopNode {
-// public:
-//     LeitorTeleopNode(ros::NodeHandle& nh) {
-        
+double current_speed = 0.0;
+const double step = 5.0; 
+const double max_speed = 100.0;
+const double min_speed = -100.0;
 
-//         sub_ = nh.subscribe("/cmd_vel", 10, &LeitorTeleopNode::teleopCallback, this);
-
-//         // Aqui você pode publicar para cada roda, se tiver tópicos separados
-//         pub_front_left_  = nh.advertise<std_msgs::Float64>("wheel/front_left/cmd_vel", 1);
-//         pub_front_right_ = nh.advertise<std_msgs::Float64>("wheel/front_right/cmd_vel", 1);
-//         pub_back_left_   = nh.advertise<std_msgs::Float64>("wheel/back_left/cmd_vel", 1);
-//         pub_back_right_  = nh.advertise<std_msgs::Float64>("wheel/back_right/cmd_vel", 1);
-
-//         ROS_INFO("No 'leitor_teleop_node' iniciado.");
-//     }
-
-// private:
-//     void teleopCallback(const geometry_msgs::Twist::ConstPtr& msg) {
-//         std_msgs::Float64 out;
-
-//         if (msg->linear.x > 0.0) {  
-//             out.data = 10.0;
-//             pub_front_left_.publish(out);
-//             pub_front_right_.publish(out);
-//             pub_back_left_.publish(out);
-//             pub_back_right_.publish(out);
-//             ROS_INFO("Frente pressionada -> rodas ligadas em 10");
-//         } else {
-//             out.data = 0.0;
-//             pub_front_left_.publish(out);
-//             pub_front_right_.publish(out);
-//             pub_back_left_.publish(out);
-//             pub_back_right_.publish(out);
-//         }
-//     }
-
-//     ros::Subscriber sub_;
-//     ros::Publisher pub_front_left_, pub_front_right_, pub_back_left_, pub_back_right_;
-// };
-
-// int main(int argc, char **argv) {
-
-//     ros::init(argc, argv, "leitor_teleop_node");
-
-//     ros::NodeHandle nh;
-
-
-//     ROS_INFO("teclado inciado");
-//     LeitorTeleopNode leitor(nh);
-
-//     ros::spin();
-//     return 0;
-// }
-
-
-
-#include "leitor_teleop_node.hpp"
 
 
 class LeitorTeleopNode {
-    public:
-        LeitorTeleopNode(ros::NodeHandle& nh, ros::NodeHandle& pnh) {
-            // Lê parâmetros (ou usa padrão)
-            pnh.param<std::string>("input_topic", input_topic_, "/cmd_vel");
-            pnh.param<std::string>("output_topic", output_topic_, "leitura/cmd_vel");
-            pnh.param<int>("queue_size", queue_size_, 10);
+public:
+    LeitorTeleopNode(ros::NodeHandle& nh) {
+        // Configura subscriber e publisher
+        sub_ = nh.subscribe("/cmd_vel", 10, &LeitorTeleopNode::teleopCallback, this);
 
-            // Configura subscriber e publisher
-            sub_ = nh.subscribe(input_topic_, queue_size_, &LeitorTeleopNode::teleopCallback, this);
-            pub_ = nh.advertise<std_msgs::Float64>(output_topic_, 1);
+        pub_front_left_  = nh.advertise<std_msgs::Float64>("/front_left_wheel/target_velocity", 1);
+        pub_front_right_ = nh.advertise<std_msgs::Float64>("/front_right_wheel/target_velocity", 1);
+        pub_back_left_   = nh.advertise<std_msgs::Float64>("/rear_left_wheel/target_velocity", 1);
+        pub_back_right_  = nh.advertise<std_msgs::Float64>("/rear_right_wheel/target_velocity", 1);
 
-            ROS_INFO_STREAM("No 'leitor_teleop_node' iniciado.");
-            ROS_INFO_STREAM("Escutando em: " << input_topic_);
-            ROS_INFO_STREAM("Publicando em: " << output_topic_);
-        }
-
-    private:
-        void teleopCallback(const geometry_msgs::Twist::ConstPtr& msg) {
-            ROS_INFO("Recebi do %s -> Linear X: %.2f | Angular Z: %.2f",
-                    input_topic_.c_str(), msg->linear.x, msg->angular.z);
-
-            std_msgs::Float64 out;
-            out.data = msg->linear.x;  // só envia Linear X
-            pub_.publish(out);
-        }
-
-        ros::Subscriber sub_;
-        ros::Publisher pub_;
-        std::string input_topic_;
-        std::string output_topic_;
-        int queue_size_;
-    };
-
-int main(int argc, char **argv) {
-
-    //melhorar e simplificar o codigo
-    ros::init(argc, argv, "leitor_teleop_node");
-    ros::NodeHandle node;
-    
-
-    ROS_INFO("INCIADO");
-
-    ros::Rate loop_rate(1); 
-
-    while (ros::ok()) {
-        ROS_INFO("No leitor_teleop_node rodando...");
-        ros::spinOnce();
-        loop_rate.sleep();
+        ROS_INFO("No 'leitor_teleop_node' iniciado.");
     }
 
+private:
+    void teleopCallback(const geometry_msgs::Twist::ConstPtr& msg) {
+        std_msgs::Float64 out;
 
+        if (msg->linear.x > 0.0) {
+            // Frente -> aumenta velocidade
+            current_speed += step;
+            if (current_speed > max_speed) current_speed = max_speed;
+            ROS_INFO("Frente pressionada -> velocidade atual %.2f", current_speed);
+        } 
+        else if (msg->linear.x < 0.0) {
+            // Trás -> diminui velocidade
+            current_speed -= step;
+            if (current_speed < min_speed) current_speed = min_speed;
+            ROS_INFO("Trás pressionada -> velocidade atual %.2f", current_speed);
+        } 
+        else {
+            // Nenhuma tecla -> mantém velocidade (ou zera se quiser)
+            // current_speed = 0; // <-- descomente se quiser parar ao soltar tecla
+        }
 
-    // LeitorTeleopNode node(node, pnh);
-    // ros::NodeHandle pnh("~");
+        out.data = current_speed;
+        pub_front_left_.publish(out);
+        pub_front_right_.publish(out);
+        pub_back_left_.publish(out);
+        pub_back_right_.publish(out);
+    }
+
+    ros::Subscriber sub_;
+    ros::Publisher pub_front_left_, pub_front_right_, pub_back_left_, pub_back_right_;
+};
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "leitor_teleop_node");
+    ros::NodeHandle nh;
+
+    LeitorTeleopNode leitor(nh);
 
     ros::spin();
     return 0;
